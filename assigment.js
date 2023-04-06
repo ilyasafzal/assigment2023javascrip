f4ed6acac7b2b895438a9c44bd93973f1aba0f0c
ilyas <muhammadilyazafzal@gmail.com>
2023-04-07 00:22:47 +0500

    remaning part 2

--- assigment.js
+++ assigment.js
@@ -41,3 +41,40 @@ if(){
 
 }
 */
+// Question 04
+/*
+for(i=0;i <= 15; i++ ){
+
+if(i%2===0 ){
+  document.write(i+" is even -> ");
+  console.log(i+" is even ");
+}else{
+  document.write(i+" is odd -> ");
+  console.log(i+" is odd ");
+}
+}
+*/
+//Question 5
+/*
+let averageMark = +prompt("Enter your obtaining ");
+if(averageMark<60){
+  document.write("The grade is F ");
+  console.log("The grade is F");
+}else if(averageMark<70){
+  document.write("The grade is D ");
+  console.log("The grade is D");
+}else if(averageMark<80){
+  document.write("The grade is C ");
+  console.log("The grade is C");
+} else if (averageMark<90){
+  document.write("The grade is B ");
+  console.log("The grade is B ");
+}else if (averageMark<100){
+  document.write("The grade is A ");
+  console.log("The grade is A ");
+}else {
+  document.write("try again ");
+  console.log("there is some problem ");
+}
+*/
+// Question 06
