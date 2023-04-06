fae544a915c58ca742a9f1c5dfd797c37b2be9a4
ilyas <muhammadilyazafzal@gmail.com>
2023-04-06 20:20:32 +0500

    asigment part 01 02

---
+++ assigment.js
@@ -0,0 +1,43 @@
+// Question 01
+/*
+let number1 = +prompt("Enter any Number");
+let number2 = +prompt("Again enter any number");
+
+if (number1 > number2){
+   console.log("This number "+ number1 +" is larger);
+}else if (number2 > number1){
+  console.log("This number "+ number2 +" is larger );
+} else if (number1 === number2 ){
+  console.log("Both number are equal--> " + number1 + " = " + number2);
+}else{
+    console.log("Try again with ineger like 1, 2 ,3,.... and so on");
+}
+*/
+
+// Question 02
+/*
+let userInput = +prompt(" Enter any number");
+let condition = userInput*10;
+let  result = condition - userInput; 
+if (condition > result){
+    alert("The sign is + " );
+}else if (condition < result){
+    alert("The sign is - " );
+}else{
+    alert("Try again ther i s some thing wrong");
+}
+*/
+
+// Question 03
+/*
+let userInput1 = +prompt("Enter any number");
+let userInput2 = +prompt("Enter any number");
+let userInput3 = +prompt("Enter any number");
+let userInput4 = +prompt("Enter any number");
+let userInput5 = +prompt("Enter any number");
+if(){
+
+}else if (){
+
+}
+*/
